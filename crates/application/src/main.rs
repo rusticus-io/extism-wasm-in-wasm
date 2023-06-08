abfd88da42ef8ed0a1c747024a2650d6e0ec9a3b
Romeo Disca <romeo.disca@gmail.com>
2023-06-08 22:55:07 +0200

    got it working

--- crates/application/src/main.rs
+++ crates/application/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeMap;
+
+use extism::*;
+
+fn main() -> anyhow::Result<()> {
+    let wasm = std::fs::read("target/wasm32-wasi/debug/pdk1.wasm")?;
+    let ctx = Context::new();
+    let mut plugin = Plugin::new(&ctx, &wasm, [], true)?;
+    let mut config = BTreeMap::new();
+    config.insert("thing".to_string(), Some("thing".to_string()));
+    plugin.set_config(&config)?;
+    let data = plugin.call("count_vowels", "this is a test")?;
+    println!("{}", String::from_utf8(data.to_vec())?);
+    assert_eq!(data, b"{\"count\":4,\"config\":\"thing\",\"a\":\"this is var a\"}");
+    Ok(())
 }
